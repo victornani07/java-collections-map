c534e8403e333c153c340b4cfffc9e99ea74507e
Viorel Contu <Viorel.Contu@endava.com>
2021-03-29 22:04:34 +0300

    initial commit for Java Collections Map Homework

---
+++ src/main/java/com/endava/internship/collections/StudentMap.java
@@ -0,0 +1,78 @@
+package com.endava.internship.collections;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class StudentMap implements Map<Student, Integer> {
+    @Override
+    public int size() {
+        //TODO
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object o) {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object o) {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public Integer get(Object o) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Integer put(Student student, Integer integer) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Integer remove(Object o) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends Student, ? extends Integer> map) {
+        //TODO
+    }
+
+    @Override
+    public void clear() {
+        //TODO
+    }
+
+    @Override
+    public Set<Student> keySet() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Collection<Integer> values() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Set<Entry<Student, Integer>> entrySet() {
+        //Ignore this for homework
+        throw new UnsupportedOperationException();
+    }
+}
+
